4ce1151ef96ccd0de977d78858ffc1de5be9c88c
sonnypainter <sonnypainter@users.noreply.github.com>
2018-10-15 10:40:15 -0500

    Add files via upload

---
+++ GameManaging.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManaging : MonoBehaviour {
+
+    public GameObject leftController;
+    public GameObject rightController;
+    public GameObject leftBox;
+    public GameObject rightBox;
+    public Slider leftSlider;
+    public Slider rightSlider;
+
+    public Text leftStartingPos;
+    public Text leftCurrentDist;
+    public Text rightStartingPos;
+    public Text rightCurrentDist;
+
+    public Text leftControllerPosX;
+    public Text leftControllerPosY;
+    public Text leftControllerPosZ;
+    public Text rightControllerPosX;
+    public Text rightControllerPosY;
+    public Text rightControllerPosZ;
+
+    private float leftDist;
+    private float rightDist;
+
+
+    // Use this for initialization
+    void Start () {
+
+        leftSlider.minValue = 10;
+        leftSlider.maxValue = 100;
+        leftSlider.wholeNumbers = true;
+        leftSlider.value = 10;
+
+        rightSlider.minValue = 10;
+        rightSlider.maxValue = 100;
+        rightSlider.wholeNumbers = true;
+        rightSlider.value = 10;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //update boxes based on slider value
+
+        leftStartingPos.text = leftSlider.value.ToString();
+        rightStartingPos.text = rightSlider.value.ToString();
+
+        leftDist = Vector3.Distance(leftController.transform.position, leftBox.transform.position);
+        rightDist = Vector3.Distance(rightController.transform.position, rightBox.transform.position);
+
+        leftCurrentDist.text = leftDist.ToString();
+        rightCurrentDist.text = rightDist.ToString();
+
+        leftControllerPosX.text = leftController.transform.position.x.ToString();
+        leftControllerPosY.text = leftController.transform.position.y.ToString();
+        leftControllerPosZ.text = leftController.transform.position.z.ToString();
+        rightControllerPosX.text = rightController.transform.position.x.ToString();
+        rightControllerPosY.text = rightController.transform.position.y.ToString();
+        rightControllerPosZ.text = rightController.transform.position.z.ToString();
+
+    }
+}
---
+++ Hand.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+
+public class Hand : MonoBehaviour {
+
+    GameObject heldObject;
+    Controller controller;
+
+    void Start()
+    {
+        controller = GetComponent<Controller>();
+    }
+
+    void Update()
+    {
+        controllerPos = transform.position;
+
+        if (heldObject)
+        {
+            if (controller.controller.GetPressUp(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
+            {
+                heldObject.transform.parent = null;
+                heldObject.GetComponent<Rigidbody>().isKinematic = false;
+                heldObject.GetComponent<HeldObject>().parent = null;
+                heldObject = null;
+            }
+        }
+        else
+        {
+            if (controller.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
+            {
+                Collider[] cols = Physics.OverlapSphere(transform.position, 0.1f);
+
+                foreach (Collider col in cols)
+                {
+                    if (heldObject == null && col.GetComponent<HeldObject>() && col.GetComponent<HeldObject>().parent == null)
+                    {
+                        heldObject = col.gameObject;
+                        heldObject.transform.parent = transform;
+                        heldObject.transform.localPosition = Vector3.zero;
+                        heldObject.transform.localRotation = Quaternion.identity;
+                        heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                        heldObject.GetComponent<HeldObject>().parent = controller;
+                    }
+                }
+            }
+        }
+    }
+}
